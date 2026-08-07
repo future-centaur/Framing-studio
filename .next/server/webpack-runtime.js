19d533c57a44276c64e0123e6cb6b23532756f4b
future-centaur <munkev001@gmail.com>
2026-08-08 02:36:48 +0300

    Refactor code structure for improved readability and maintainability

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("86b1a1bb90c0e3a0")
+/******/ 		__webpack_require__.h = () => ("6b2307be8ea50c22")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
